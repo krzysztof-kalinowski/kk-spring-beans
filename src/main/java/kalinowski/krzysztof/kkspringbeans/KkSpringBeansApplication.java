2dc09a8c096b4ab5ad79206f28f24ff6fd82cc77
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2022-03-05 17:17:36 +0100

    init commit

---
+++ src/main/java/kalinowski/krzysztof/kkspringbeans/KkSpringBeansApplication.java
@@ -0,0 +1,13 @@
+package kalinowski.krzysztof.kkspringbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkSpringBeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkSpringBeansApplication.class, args);
+    }
+
+}
